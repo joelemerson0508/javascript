be014107dd33f7678daaf8bfe897c77ac36d9d90
joelemerson0508 <131798852+joelemerson0508@users.noreply.github.com>
2023-07-01 00:38:05 -0300

    if e else

---
+++ js10/ex01.js
@@ -0,0 +1,26 @@
+let energia = 100
+let clima = "chovendo"
+
+if(energia > 70 && clima == "sol") {
+    console.log("Vou à praia")
+} else {
+    console.log("Vou ao cinema")
+}
+
+
+/*let num = 100
+
+if (num > 10) {
+    console.log("Numeral maior que 10")
+    if(num > 50) {
+        console.log("Numeral maior que 50")
+    }
+} else if(num > 5) {
+    console.log("Numeral está entre 6 e 10")
+} else {
+    console.log("Numeral menor ou igual a 5")
+} */
+
+
+console.log("Fim do programa")
+
