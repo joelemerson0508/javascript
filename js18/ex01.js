fa75762608f2575c939c44b67cdb448e52275e12
joelemerson0508 <131798852+joelemerson0508@users.noreply.github.com>
2023-07-06 22:41:00 -0300

    atualizei javascript

---
+++ js18/ex01.js
@@ -0,0 +1,13 @@
+function canal() {
+    let n1 = 2
+    let n2 = 10
+    let res = n1*n2
+    if(res%2 == 0)
+        return "Par"
+    else
+        return "Impar"
+}
+
+let res = canal()
+
+console.log(res)
